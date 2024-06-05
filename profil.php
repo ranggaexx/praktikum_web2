8838178a25e57d37db9474243aabce3bff8b2afa
ranggaexx <mranggaofficial@gmail.com>
2024-06-05 14:30:34 +0800

    hasil praktikkum web2 2024

---
+++ profil.php
@@ -0,0 +1,44 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/Angga.jpg" alt="Angga" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2210010638</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Muhammad Rangga Maulana</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 10 Mei 2004</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0831 1475 3561</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">mranggaofficial@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://www.instagram.com/ranggaaex" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
